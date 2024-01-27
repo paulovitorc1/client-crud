d0c8518b179749f344053e0f2faa56f7c91ca055
Paulo Carneiro <paulovitorcarneiro@hotmail.com>
2024-01-26 22:09:56 -0300

    Project created.

---
+++ src/main/java/com/softwarehouse/ClientCrudApplication.java
@@ -0,0 +1,13 @@
+package com.softwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwarehouse/ClientCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
